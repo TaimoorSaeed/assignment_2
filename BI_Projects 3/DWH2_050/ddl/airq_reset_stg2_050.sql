53e1afe2ea2eb4893c7acfea269c65478a8accc7
TaimoorSaeed <taimoor.dev@gmail.com>
2025-12-04 17:33:20 +0100

    added initial project

---
+++ BI_Projects 3/DWH2_050/ddl/airq_reset_stg2_050.sql
@@ -0,0 +1,5 @@
+-- -------------------------------
+-- 1) Assignment 1: create/reset stg2_xxx schema per group
+-- -------------------------------
+DROP SCHEMA IF EXISTS stg2_050 CASCADE;
+CREATE SCHEMA stg2_050 AUTHORIZATION grp_050;
---
+++ BI_Projects 3/DWH2_050/etl/a2_etl03_dim_param.sql
@@ -0,0 +1,26 @@
+-- Assignment 2 ETL: dim_param
+-- HINT: Pull from stg_050.tb_param:
+--   param_name, param_code (optional), category, purpose, unit
+-- EXAMPLE SHAPE:
+-- TRUNCATE TABLE dwh2_050.dim_param;
+-- INSERT INTO dwh2_050.dim_param (...)
+-- SELECT p.paramname, NULL, p.category, p.purpose, p.unit FROM stg_xxx.tb_param p;
+
+-- Make A2 dwh2_xxx, stg2_xxx schemas the default for this session
+SET search_path TO dwh2_050, stg2_050;
+
+-- =======================================
+-- Load dim_param
+-- =======================================
+
+-- Step 1: Truncate target table - dim_param
+TRUNCATE TABLE dim_param RESTART IDENTITY CASCADE;
+
+-- Step 2: Insert data into the dim_param
+-- Param (Purpose → Category → Param)
+INSERT INTO dim_param (param_name, param_code, category, purpose, unit)
+SELECT p.paramname, NULL, p.category, p.purpose, p.unit
+FROM tb_param p
+ORDER BY p.id;
+
+
